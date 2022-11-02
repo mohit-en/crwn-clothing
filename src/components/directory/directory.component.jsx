fab07c05fb2ebece5de4ef8ee5767b8473a65c76
mohit <mohitmistry1758@gmail.com>
2022-11-02 09:17:09 +0530

    basic home page is completed

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import MenuItem from "../menu-items/menu-items.components";
+import "./directory.component.scss";
+
+export default class Directory extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sections: [
+        { title: "hats", imageUrl: "https://i.ibb.co/cvpntL1/hats.png", id: 1 },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+        },
+        {
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          id: 4,
+          size: "large",
+        },
+        {
+          title: "mens",
+          size: "large",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          id: 5,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
---
+++ src/components/menu-items/menu-items.components.jsx
@@ -0,0 +1,21 @@
+import "./menu-items.components.scss";
+import React from "react";
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="content">
+        <div className="title">{title.toUpperCase()}</div>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
